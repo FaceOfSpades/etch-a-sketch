a2de2acd9dbe0b50e862f279439326a67024d333
Casey <141671253+FaceOfSpades@users.noreply.github.com>
2023-10-06 13:04:21 -0400

    Fix grid alignment issue
    removed 2nd function of appending to each div and appended all square
    to the same div, then used flex wrap to line the grid up correctly

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
 const grid = document.querySelector("#container");
 
 function createRows() {
-    for (i = 0; i < 16; i++)
+    for (i = 0; i < 256; i++)
     {
         const square = document.createElement("div");
-        square.setAttribute("style", "border: 5px solid black; width: 50px; height: 50px;");
-        square.textContent = "TEST" + i;
+        square.setAttribute("style", "border: 2px solid black; width: 50px; height: 50px;");
+        square.id = "row" + i;
+        square.classList.add("rows");
         grid.appendChild(square);
     }
 }
 
-function createColumns() {
-    for (i = 0; i < 16; i++)
-    {
-        createRows();
-        const p = document.createElement("p");
-        grid.appendChild(p);
-    }
-}
-
-createColumns();
+createRows();
