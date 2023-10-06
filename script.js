be118dde36fd9c2cb3ca4c035ac55d6f834f71b9
Casey <141671253+FaceOfSpades@users.noreply.github.com>
2023-10-06 18:20:12 -0400

    Finish display of grid with proper sizing
    set the total size of the container and adjusted the size of the
    square divs to fill it according with how many square there were

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
 const grid = document.querySelector("#container");
+const h1 = document.querySelector("h1");
+const button = document.createElement("button");
+button.textContent = "Change Number of Grid Squares";
+h1.appendChild(button);
+button.setAttribute("style", "padding: 5px;");
+const display = document.createElement("div");
+display.setAttribute("style", "font-weight: normal; font-size: 22px;");
 
-function createRows() {
-    for (i = 0; i < 256; i++)
+button.addEventListener("click", () => {
+    let number = prompt("Please enter a number for total squares per side that does not exceed 100", "16");
+    removeAllChildNodes(grid);
+    createGrid(number);
+});
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    };
+};
+
+function createGrid(x) {
+    display.remove();
+    display.textContent = "Current Grid Size: " + x + "x" + x;
+    h1.appendChild(display);
+    let temp = x;
+    temp = 1000/temp;
+    x *= x;
+    for (i = 0; i < x; i++)
     {
         const square = document.createElement("div");
-        square.setAttribute("style", "border: 2px solid black; width: 50px; height: 50px;");
-        square.id = "row" + i;
-        square.classList.add("rows");
+        square.style.width = (temp) + "px";
+        square.style.height = (temp) + "px";
         grid.appendChild(square);
-    }
-}
+    };
+};
 
-createRows();
+createGrid(16);
+
+grid.addEventListener("mouseover", (event) => {
+    
+});
