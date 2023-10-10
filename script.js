17a611a300a2a65d224b74b4db4195bbb7b11799
Casey <141671253+FaceOfSpades@users.noreply.github.com>
2023-10-10 15:26:12 -0400

    Complete project
    added event listener for mouseover to each div when creating grid as
    well as a mousedown listener to turn on and mouseup to turn off for
    whole grid to allow better drawing

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
 const grid = document.querySelector("#container");
 const h1 = document.querySelector("h1");
 const button = document.createElement("button");
-button.textContent = "Change Number of Grid Squares";
-h1.appendChild(button);
-button.setAttribute("style", "padding: 5px;");
+const resetButton = document.createElement("button");
+const buttonContainer = document.createElement("div");
 const display = document.createElement("div");
+
+button.textContent = "Change Number of Grid Squares";
+resetButton.textContent = "Reset";
+
+h1.appendChild(buttonContainer);
+buttonContainer.appendChild(button);
+buttonContainer.appendChild(resetButton);
+buttonContainer.setAttribute("style", "display: flex; gap: 8px;");
+button.setAttribute("style", "padding: 5px;");
+resetButton.setAttribute("style", "padding: 5px;");
+
 display.setAttribute("style", "font-weight: normal; font-size: 22px;");
 
 button.addEventListener("click", () => {
-    let number = prompt("Please enter a number for total squares per side that does not exceed 100", "16");
-    removeAllChildNodes(grid);
-    createGrid(number);
+    let number = prompt("Please enter a number for total squares per side between 2 and 100", "16");
+    if (number > 100 || number < 2)
+    {
+        alert("That is not a valid number, please try again.");
+    } else {
+        removeAllChildNodes(grid);
+        createGrid(number);
+    };
+});
+
+resetButton.addEventListener("click", () => {
+    const boxes = document.querySelectorAll("div");
+    boxes.forEach((div) => {
+        div.style.backgroundColor = "black";
+    });
 });
 
 function removeAllChildNodes(parent) {
@@ -31,12 +53,24 @@ function createGrid(x) {
         const square = document.createElement("div");
         square.style.width = (temp) + "px";
         square.style.height = (temp) + "px";
+        let value = false;
+        
+        grid.addEventListener("mousedown", () => {
+            value = true;
+        });
+        grid.addEventListener("mouseup", () => {
+            value = false;
+        });
+        
+        square.addEventListener("mouseover", () => {
+            if (value == true)
+            {
+                square.style.backgroundColor = "white";
+            };
+        });
+
         grid.appendChild(square);
     };
 };
 
 createGrid(16);
-
-grid.addEventListener("mouseover", (event) => {
-    
-});
